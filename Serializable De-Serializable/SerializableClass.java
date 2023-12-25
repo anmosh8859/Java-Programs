f4c4666135241ec5b6ddb566610e90be738122bc
anmosh8859 <anandmsharma786@gmail.com>
2023-12-26 02:46:14 +0530

    Serializable De-Serializable Added;

---
+++ Serializable De-Serializable/SerializableClass.java
@@ -0,0 +1,40 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class SerializableClass implements Serializable {
+    private int i = 2;
+    private int j = 3;
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    SerializableClass(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fileStream = new FileOutputStream("Serial.txt");
+        ObjectOutputStream objStream = new ObjectOutputStream(fileStream);
+        objStream.writeObject(new SerializableClass(3, 5));
+        objStream.close();
+
+        FileInputStream input = new FileInputStream("Serial.txt");
+        ObjectInputStream objInput = new ObjectInputStream(input);
+        SerializableClass serializableClass = (SerializableClass) objInput.readObject();
+        System.out.println("i: " + serializableClass.getI());
+        System.out.println("j: " + serializableClass.getJ());
+        objInput.close();
+    }
+
+}
